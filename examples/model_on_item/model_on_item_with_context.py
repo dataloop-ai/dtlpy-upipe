2584dcc59367ee12fe0988b06d642bf34e4490b1
Or Shabtay <or@dataloop.ai>
2021-12-05 20:26:33 +0200

    added model and item example

---
+++ examples/model_on_item/model_on_item_with_context.py
@@ -0,0 +1,79 @@
+import numpy as np
+import dtlpy as dl
+import asyncio
+import cv2
+
+from upipe import Processor, Pipe, DType
+
+
+class DummyModel:
+    def predict(self, image):
+        return [0, 0, 0, 0, 1]
+
+
+async def on_item():
+    print("loading init")
+    me = Processor("on_item")
+    await me.connect()
+    print("on_item connected")
+    while True:
+        msg = await me.get_sync(timeout=np.inf)
+        item_id: dl.Item = msg['item_id']
+        print('got item id: {}'.format(item_id))
+        item = dl.items.get(item_id=item_id)
+        buffer = item.download(save_locally=False)
+        bgr = cv2.imdecode(np.frombuffer(buffer.read(), np.uint8), -1)
+        msg['image'] = bgr
+        await me.emit(msg, DType.JSON)
+
+
+async def age_detection():
+    print("loading age detector")
+    me = Processor("age_detection")
+    await me.connect()
+    print("age_detection connected")
+    age_detector = DummyModel()
+    while True:
+        msg = await me.get_sync(timeout=np.inf)
+        image = msg['image']
+        predictions = age_detector.predict(image=image)
+        msg['first_predictions'] = predictions
+        await me.emit(msg, DType.JSON)
+
+
+async def nude_net():
+    print("Hello nude_net")
+    me = Processor("nude_net")
+    await me.connect()
+    me.start()
+    nude_net_module = DummyModel()
+    while True:
+        msg = await me.get_sync(timeout=np.inf)
+        image = msg['image']
+        predictions = nude_net_module.predict(image=image)
+        msg['second_predictions'] = predictions
+        await me.emit(msg, DType.JSON)
+
+
+async def main():
+    a = Processor('on_item', func=on_item)
+    b = Processor('age_detection', func=age_detection)
+    c = Processor('nude_net', func=nude_net)
+    pipe = Pipe('content-detection')
+    pipe.add(a).add(b).add(c)
+    await pipe.start()
+    await pipe.wait_for_completion()
+    print("Running")
+
+
+async def to_run():
+    pipe = Pipe('content-detection')
+
+    future = asyncio.run(pipe.emit(data={'item_id': '617fd6f591e01807b4adef92'},
+                                   d_type=DType.JSON
+                                   ))
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
